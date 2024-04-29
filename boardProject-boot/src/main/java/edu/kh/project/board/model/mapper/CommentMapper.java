243b1269eed46a98f15670cbe45f59862ed2dcb7
shong69 <shong7576@gmail.com>
2024-04-29 12:53:25 +0900

    comment

---
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/mapper/CommentMapper.java
@@ -0,0 +1,18 @@
+package edu.kh.project.board.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.board.model.dto.Comment;
+
+@Mapper
+public interface CommentMapper {
+
+	/** 댓글 목록 조회
+	 * @param boardNo
+	 * @return List<Comment>
+	 */
+	List<Comment> select(int boardNo);
+
+}
